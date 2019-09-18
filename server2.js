075ced4947cf5b7d4567457460a32af71737691a
Tony Bedford <tony.bedford@vonage.com>
2019-09-18 13:22:18 +0100

    Inbound call

---
+++ server2.js
@@ -0,0 +1,49 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 9000; // change port number as required
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const ncco = [
+  {
+    "action": "talk",
+    "text": "Please wait while we connect you to an agent"
+  },
+  {
+    "action": "connect",
+    "eventUrl": [
+      "https://tbedford.ngrok.io/webhooks/rtcevent"
+    ],
+    "from": "442039061241",
+    "endpoint": [
+      {
+        "type": "app",
+        "user": "tonyb"
+      }
+    ]
+  }
+]
+
+app.get('/webhooks/answer', (req, res) => {
+    console.log("Answer:");
+    console.log(req.query);
+    var dest_number = req.query.to;
+    res.json(ncco);
+});
+
+app.post('/webhooks/event', (req, res) => {
+    console.log("EVENT:");
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.post('/webhooks/rtcevent', (req, res) => {
+    console.log("RTC_EVENT:");
+    console.log(req.body);
+    res.status(200).end();
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
